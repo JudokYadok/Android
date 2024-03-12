28d2df5a75d5eaf1cf07e8fb93f65a9faf913e50
yummpotato <hevlyoy26@naver.com>
2024-03-12 20:21:28 +0900

    Add: 카카오 로그인 구현

--- app/src/main/java/com/example/nunettine/ui/main/LoginActivity.kt
+++ app/src/main/java/com/example/nunettine/ui/main/LoginActivity.kt
@@ -2,11 +2,23 @@ package com.example.nunettine.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nunettine.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity: AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private var nickname : String? = ""
+    private var profile :String? = ""
+    private var email: String? = ""
+    private var accessToken: String? = ""
+    private var member_id: Int? = 0
+    private var getAccessToken: String? = ""
+    private var getRefreshToken: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +26,62 @@ class LoginActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         binding.loginKakaoBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            login_kakao()
         }
+    }
+
+    private fun login_kakao() {
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        // 이메일 로그인 콜백
+        val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e(TAG, "로그인 실패 $error")
+            } else if (token != null) {
+                Log.e(TAG, "로그인 성공 ${token.accessToken}")
+
+                UserApiClient.instance.me { user, _ ->
+                    if (user != null) {
+                        nickname = user.kakaoAccount?.profile?.nickname.toString()
+                        profile = user.kakaoAccount?.profile?.profileImageUrl.toString()
+                        email = user.kakaoAccount?.email
+                        accessToken = token.accessToken
+                    }
+                }
+                goMainActivity()
+            }
+        }
+
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            // 카카오톡 로그인
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                // 로그인 실패 부분
+                if (error != null) {
+                    Log.e(TAG, "로그인 실패 $error")
+                    // 사용자가 취소
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
+                        return@loginWithKakaoTalk
+                    }
+                    // 다른 오류
+                    else {
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
+                    }
+                }
+                // 로그인 성공 부분
+                else if (token != null) {
+                    Log.e(TAG, "로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
+        }
+    }
+
+    private fun goMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        var TAG = "LoginActivity"
     }
 }
