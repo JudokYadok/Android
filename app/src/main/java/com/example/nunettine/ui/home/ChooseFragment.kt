67ce83b110fe2179069c90ba8a955507ad652681
yummpotato <hevlyoy26@naver.com>
2024-03-19 15:54:15 +0900

    Add: 문제 유형 view 제작

---
+++ app/src/main/java/com/example/nunettine/ui/home/ChooseFragment.kt
@@ -0,0 +1,17 @@
+package com.example.nunettine.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.nunettine.databinding.FragmentChooseBinding
+
+class ChooseFragment: Fragment() {
+    private lateinit var binding: FragmentChooseBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentChooseBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/nunettine/ui/home/TypeFragment.kt
+++ app/src/main/java/com/example/nunettine/ui/home/TypeFragment.kt
@@ -1,27 +1,23 @@
 package com.example.nunettine.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.nunettine.R
-import com.example.nunettine.databinding.FragmentHomeBinding
+import com.example.nunettine.databinding.FragmentTypeBinding
 
-class HomeFragment: Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class TypeFragment: Fragment() {
+    private lateinit var binding: FragmentTypeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentTypeBinding.inflate(layoutInflater)
         clickListener()
         return binding.root
     }
 
     private fun clickListener() = with(binding) {
-        homeSolveBtn.setOnClickListener {
-            moveFragment(TypeFragment())
-        }
     }
 
     private fun moveFragment(fragment: Fragment) {
