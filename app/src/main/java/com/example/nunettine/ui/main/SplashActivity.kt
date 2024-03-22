cad3c3d0f046e2d59a3441ac7684cddc0ce5d976
yummpotato <hevlyoy26@naver.com>
2024-03-22 12:24:55 +0900

    Add: splash animation 추가 및 앱 아이콘, 이름 변경

--- app/src/main/java/com/example/nunettine/ui/main/SplashActivity.kt
+++ app/src/main/java/com/example/nunettine/ui/main/SplashActivity.kt
@@ -3,7 +3,10 @@ package com.example.nunettine.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nunettine.R
 import com.example.nunettine.databinding.ActivitySplashBinding
 
 class SplashActivity: AppCompatActivity() {
@@ -13,6 +16,10 @@ class SplashActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.gradient_expand_animation)
+        binding.splashTv.startAnimation(animation)
+        binding.splashIco.startAnimation(animation)
 
         Handler().postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
